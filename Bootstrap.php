5dff147e67621f5da1b162cd1a1c9ff4e0bd97f6
bohneGit <swaldhelm@gmx.de>
2016-07-22 09:48:04 +0200

    Update Bootstrap.php

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,1 +1,45 @@
-
+<?php 
+class Shopware_Plugins_Backend_BackendBoilerplate_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+	public function getInfo()
+	{
+		return array(
+				'label' => 'Backend Boilerplate';
+		);
+	}
+	
+	public function install()
+	{
+		$this->subscribeEvent(
+			'Enlight_Controller_Dispatcher_ControllerPath_Backend_BackendBoilerplate',
+			'getBackendController'	
+		);
+		
+		$this->createMenuItem(array(
+				'label' => 'Backend Boilerplate',
+				'controller' => 'Backend Boilerplate',
+				'class' => 'sprite-application-block',
+				'action' => 'Index',
+				'active' => 1,
+				'parent' => $this->Menu()->findOneBy(['label' => 'Marketing'])
+		));
+		return true;
+	}
+	
+	public function uninstall()
+	{
+		return true;
+	}
+	
+	public function getBackendController(Enlight_Event_EventArgs $args)
+	{
+		$this->Application()->Template()->addTemplateDir(
+			$this->Path() . 'Views/'	
+		);
+		
+		$this->registerCustomModels();
+		
+		return $this->Path() . '/Controllers/Backend/Backend Boilerplate.php';
+	}
+}
+?>
